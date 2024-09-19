eb2a102330383255d1587afe1a7757704956ae0d
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-09-19 21:32:55 +0100

    feat: Prettier Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import path from 'path';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 
 export default tseslint.config({
@@ -13,7 +14,7 @@ export default tseslint.config({
   files: ['**/*.ts'],
   extends: [
     eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.recommendedTypeChecked, eslintConfigPrettier
   ],
   rules: {
     "no-console": "error",
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
 // console.log('Hello World');
+
+const name = 'Praise';
+
+if (name === 'Praise') {
+console.log('Hello Praise');
+} else {
+    console.log('Hello World');
+}
