53eceebdb18e4cc004b68add1e66b97e58454dc0
Nwaoghor Praise Chukunweiken <chukunwaoghor@gmail.com>
2024-09-22 00:38:28 +0100

    feat: Setup Color Log Terminal

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,47 +1,112 @@
-import {createLogger, format, transports} from 'winston';
-import { ConsoleTransportInstance } from 'winston/lib/winston/transports';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { createLogger, format, transports } from 'winston';
+import { ConsoleTransportInstance, FileTransportInstance } from 'winston/lib/winston/transports';
 import util from 'util';
+import config from '../config/config';
+import { ApplicationENV } from '../constant/application';
+import path from 'path';
+import * as sourceMapSupoort from 'source-map-support';
+import {red, blue, yellow, green, magenta} from 'colorette';
+
+sourceMapSupoort.install();
+
 
 class Logger {
+    private colorizeLevel(level: string): string {
+        switch (level) {
+            case 'ERROR':
+                return red(level);
+            case 'WARN':
+                return yellow(level);
+            case 'INFO':
+                return blue(level);
+            default:
+                return level;
+        }
+    }
+
+
     private consoleLogFormat = format.printf((info) => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+         
         const { timestamp, level, message, meta = {}} = info;
 
-        const customLevel = level.toUpperCase();
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const customTimeStamp = timestamp;
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const customLevel = this.colorizeLevel(level.toUpperCase());
+         
+        const customTimeStamp = green(timestamp as string);
+         
         const customMessage = message;
-        const customMeta = util.inspect(meta, { showHidden: false, depth: null });
+        const customMeta = util.inspect(meta, { showHidden: false, depth: null, colors: true });
 
-        const customLog = `${customLevel} - [${customTimeStamp}]: ${customMessage}\n${'META - '} ${customMeta}\n`
+        const customLog = `${customLevel} - [${customTimeStamp}]: ${customMessage}\n${magenta('META - ')} ${customMeta}\n`;
 
         return customLog;
     });
 
+    private fileLogFormat = format.printf((info) => {
+         
+        const { timestamp, level, message, meta = {}} = info;
+
+        const logMeta: Record<string, unknown> = {};
+
+        for (const [key, value] of Object.entries(meta)){
+            if (value instanceof Error){
+                logMeta[key] = {
+                    name: value.name,
+                    message: value.message,
+                    trace: value.stack || ''
+                };
+            } else {
+                logMeta[key] = value;
+            }
+        }
+
+        const logData = {
+            level: level.toUpperCase(),
+            timestamp,
+            message,
+            meta: logMeta
+        };
+
+        return JSON.stringify(logData, null, 4);
+    });
 
     private consoleTransport = (): Array<ConsoleTransportInstance> => {
+        if (config.ENV === ApplicationENV.DEVELOPMENT){
+            return [
+                new transports.Console({
+                    level: 'info',
+                    format: format.combine(
+                        format.timestamp(),
+                        this.consoleLogFormat
+                    )
+                })
+            ];
+        }
+
+        return [];
+    };
+
+    private fileTransport = (): Array<FileTransportInstance> => {
         return [
-            new transports.Console({
+            new transports.File({
+                filename: path.join(__dirname, '../', '../', 'logs', `${config.ENV}.log`),
                 level: 'info',
                 format: format.combine(
                     format.timestamp(),
-                    this.consoleLogFormat
+                    this.fileLogFormat
                 )
             })
-
         ];
     };
 
-
     public logger = createLogger({
-        defaultMeta:{
-            meta: {
-
-            }
+        defaultMeta: {
+            meta: {}
         },
-        transports:[
-            ...this.consoleTransport()
+        transports: [
+            ...this.consoleTransport(),
+            ...this.fileTransport()
         ]
     });
 }
