da2e539600cfd295c075b31106ba0de5f9a724b2
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2024-11-10 22:55:54 +0000

    chore: OpenAI

--- src/utils/ai-helper/message-generator.ts
+++ src/utils/ai-helper/message-generator.ts
@@ -1,3 +1,18 @@
-// class MessageService {
+import OpenAI from 'openai';
+class MessageService {
+    private openai: OpenAI;
+    constructor() {
+        this.openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY
+        });
+    }
 
-// }
+    async createMessage(content: string) {
+        const chatCompletion = await this.openai.chat.completions.create({
+            messages: [{role: 'user', content: content}],
+            model: 'gpt-3.5-turbo'
+        });
+        return chatCompletion.choices[0].message.content;
+    }
+}
+
