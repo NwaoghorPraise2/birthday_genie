802f36a744c966bfd493aaf322550d155d1f8dc3
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2025-02-13 19:38:33 +0000

    chore: Configure AI Model Handlers

--- src/lib/openAi/openAiService.ts
+++ src/lib/openAi/openAiService.ts
@@ -1,1 +1,39 @@
-export default class OpenAiService {}
+import OpenAi from 'openai';
+import config from '../../config/config';
+
+class OpenAiService {
+    private openai: OpenAi;
+
+    constructor() {
+        this.openai = new OpenAi({
+            apiKey: config.OPEN_AI_SECRET
+        });
+    }
+
+    async generateMessage(prompt: string): Promise<string> {
+        const response = await this.openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: prompt
+                }
+            ]
+        });
+        return response.choices[0].message.content as string;
+    }
+
+    async generateStreamChat(prompt: string) {
+        const stream = await this.openai.chat.completions.create({
+            model: 'gpt-4',
+            messages: [{role: 'user', content: prompt}],
+            stream: true
+        });
+        for await (const chunk of stream) {
+            process.stdout.write(chunk.choices[0]?.delta?.content || '');
+        }
+    }
+}
+
+export default new OpenAiService();
+
